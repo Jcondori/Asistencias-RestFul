5f6b0867dc5527a090319571ef7bbc4306264d2d
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2018-11-17 15:02:39 -0500

    Estructura de Proyecto

---
+++ src/main/java/com/vallegrande/asistencias/AsistenciasApplication.java
@@ -0,0 +1,12 @@
+package com.vallegrande.asistencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsistenciasApplication.class, args);
+    }
+}
