12203c60317ffcbdaac734ef3819552097fbfd3a
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2018-11-19 16:33:31 -0500

    desarrollo del algoritmo de encriptacion del cesar

--- src/main/java/com/vallegrande/asistencias/Encriptacion.java
+++ src/main/java/com/vallegrande/asistencias/Encriptacion.java
@@ -7,6 +7,7 @@ import java.security.NoSuchAlgorithmException;
 public class Encriptacion {
 
     public static String encriptar(String input) {
+//        input = cifrar(input,5);
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] messageDigest = md.digest(input.getBytes());
@@ -20,6 +21,20 @@ public class Encriptacion {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String cifrar(String texto, int desplazamiento) {
+        String iMayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String iMinus = "abcdefghijklmnopqrstuvwxyz";
+        String salida = "";
+        for (int i = 0; i < texto.length(); i++) {
+            if ((iMayus.indexOf(texto.charAt(i)) != -1) || (iMinus.indexOf(texto.charAt(i)) != -1)) {
+                salida += (iMayus.indexOf(texto.charAt(i)) != -1) ? iMayus.charAt((iMayus.indexOf(texto.charAt(i)) + desplazamiento)%iMayus.length()) : iMinus.charAt((iMinus.indexOf(texto.charAt(i)) + desplazamiento)%iMinus.length());
+            }else{
+                texto += texto.charAt(i);
+            }
+        }
+        return salida;
     }
 
     public static void main(String[] args) {
